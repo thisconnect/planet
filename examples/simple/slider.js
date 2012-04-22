9c9708b8eee8d3853e02c176fed0f34c0ea9716d
tcme <hi@this-connect.me>
2012-04-22 11:51:39 +0200

    example cleanup round II

---
+++ examples/simple/slider.js
@@ -0,0 +1,46 @@
+(function(){
+
+var mySlider = new Slider($('slider'), $('knob'), {
+	offset: -2,
+	onChange: function(step){
+		socket.send(JSON.stringify({
+			type: 'attempt_update',
+			payload: {
+				component: 'slider',
+				payload: step
+			}
+		}));
+	}
+});
+
+mySlider.element.addEvent('mousedown', function(){
+	socket.send(JSON.stringify({
+		type: 'attempt_update',
+		payload: {
+			component: 'slider',
+			payload: mySlider.step
+		}
+	}));
+});
+
+socket.on('message', function(data){
+	data = JSON.parse(data);
+	
+	if (data.payload['slider'] == null) return;
+
+	if (data.type == 'state_update'){
+		mySlider.setKnobPosition(mySlider.toPosition(data.payload['slider']));
+		mySlider.step = data.payload['slider'];
+	}
+
+	if (data.type == 'initial_state'){
+		mySlider.setKnobPosition(mySlider.toPosition(data.payload['slider']));
+		mySlider.step = data.payload['slider'];
+	}
+});
+
+
+})();
+
+
+
