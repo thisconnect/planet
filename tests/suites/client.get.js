946f0b05f62fcec77dc90d8d90c6ed84c616f9ee
tcme <hi@this-connect.me>
2012-10-19 17:56:39 +0200

    get by path

--- tests/suites/client.get.js
+++ tests/suites/client.get.js
@@ -64,22 +64,34 @@ Tests.describe('Planet API: Get', function(it){
 	});
 
 
-	it('should `get` by key', function(expect){
-		expect.perform(4);
+	it('should emit `get` search a value by key', function(expect){
+		expect.perform(13);
 
 		var first = io.connect('//:8999', {'force new connection': true});
 
 		first.on('connect', function(){
-			first.emit('put', { a: { b: { c: 0 } } });
+			first.emit('put', {a: {b: {c: 0}}});
 		});
 
 		first.on('put', function(data){
-			this.disconnect();
+			first.disconnect();
 		});
 
 		first.on('disconnect', function(){
 
 			var second = io.connect('//:8999', {'force new connection': true});
+
+			second.on('error', function(){
+				throw new Error('client error');
+			});
+
+			second.emit('get', function(data){
+				expect(data).toBeType('object');
+				expect(data.a).toBeType('object');
+				expect(data.a.b).toBeType('object');
+				expect(data.a.b.c).toBeType('number');
+				expect(data.a.b.c).toBe(0);
+			});
 
 			second.emit('get', 'a', function(data){
 				expect(data).toBeType('object');
@@ -88,9 +100,95 @@ Tests.describe('Planet API: Get', function(it){
 				expect(data.b.c).toBe(0);
 			});
 
-			second.emit('get', 'b', function(data){
+			second.emit('get', ['a', 'b'], function(data){
+				expect(data).toBeType('object');
+				expect(data.c).toBeType('number');
+				expect(data.c).toBe(0);
+			});
+
+			second.emit('get', ['a', 'b', 'c'], function(data){
+				expect(data).toBe(0);
+				second.disconnect();
+			});
+		});
+	});
+
+
+	it('should return null for nonexistent or invalid keys', function(expect){
+		expect.perform(4);
+
+		var first = io.connect('//:8999', {'force new connection': true});
+
+		first.on('connect', function(){
+			first.emit('put', {a: {b: {c: 0}}});
+		});
+
+		first.on('put', function(data){
+			first.disconnect();
+		});
+
+		first.on('disconnect', function(){
+
+			var second = io.connect('//:8999', {'force new connection': true});
+
+			second.on('error', function(){
+				throw new Error('client error');
+			});
+
+			second.emit('get', 'd', function(data){
 				expect(data).toBeNull();
-				this.disconnect();
+			});
+
+			second.emit('get', 1, function(data){
+				expect(data).toBeNull();
+			});
+
+			second.emit('get', [null], function(data){
+				expect(data).toBeNull();
+			});
+
+			second.emit('get', ['no', 'key', 'here'], function(data){
+				expect(data).toBeNull();
+			});
+
+			second.emit('get', new Date(), function(data){
+				expect(data).toBeNull();
+				second.disconnect();
+			});
+		});
+	});
+
+
+	it('should error on invalid keys', function(expect){
+		expect.perform(1);
+		var spy = new Spy();
+
+		var first = io.connect('//:8999', {'force new connection': true});
+
+		first.on('connect', function(){
+			first.emit('put', {a: {b: {c: 0}}});
+		});
+
+		first.on('put', function(data){
+			first.disconnect();
+		});
+
+		first.on('disconnect', function(){
+
+			var second = io.connect('//:8999', {'force new connection': true});
+
+			second.on('error', function(){
+				spy();
+				if (spy.getCallCount() == 4) second.disconnect();
+			});
+
+			second.emit('get', null, function(data){});
+			second.emit('get', undefined, function(data){});
+			second.emit('get', [], function(data){});
+			second.emit('get', {}, function(data){});
+
+			second.on('disconnect', function(){
+				expect(spy.getCallCount()).toBe(4);
 			});
 		});
 	});
