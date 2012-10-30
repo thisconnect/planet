33918460f689e964add72b2e440c783557053541
tcme <hi@this-connect.me>
2012-10-30 07:51:12 +0100

    reenable all tests (wip async tests currently interfere)

--- tests/suites/tests.js
+++ tests/suites/tests.js
@@ -2,13 +2,13 @@ if (this.window == null) io = require('../../node_modules/socket.io/node_modules
 
 exports.setup = function(tests){
 
-	// require('./client.connect').setup(tests);
-	// require('./client.put').setup(tests);
-	// require('./client.post').setup(tests);
-	// require('./client.get').setup(tests);
-	// require('./client.delete').setup(tests);
-	// require('./client.remove').setup(tests);
+	require('./client.connect').setup(tests);
+	require('./client.put').setup(tests);
+	require('./client.post').setup(tests);
+	require('./client.get').setup(tests);
+	require('./client.delete').setup(tests);
+	require('./client.remove').setup(tests);
 	require('./client.array').setup(tests);
-	// require('./client.stress').setup(tests);
+	require('./client.stress').setup(tests);
 
 };
