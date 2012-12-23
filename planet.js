ff1e9805153ea5d91fa3f029ac30f57a13a2edeb
tcme <hi@this-connect.me>
2012-12-23 10:22:53 +0100

    dont expose internal methods

--- planet.js
+++ planet.js
@@ -11,54 +11,18 @@ function Planet(io, options){
 	this.limit = options.limit || 200;
 
 	this.server = io.server
-		.on('listening', this.onListening.bind(this))
-		.on('clientError', this.onError.bind(this));
+		.on('listening', listen.bind(this))
+		.on('clientError', error.bind(this));
 
 	this.sockets = io.sockets
-		.on('connection', this.onConnection.bind(this));
+		.on('connection', connect.bind(this));
 
 }
 
 Planet.prototype = Object.create(Emitter.prototype);
 
-Planet.prototype.onListening = function(){
-	var location = this.server.address();
-	log('Planet started at ' + [location.address, location.port].join(':'));
-	// this.emit('listening', this, location.address, location.port);
-};
-
-Planet.prototype.onError = function(error){
-	log('error', error);
-};
-
+Planet.prototype.state = {};
 Planet.prototype.count = 0;
-
-Planet.prototype.onConnection = function(conn){
-	this.count++;
-	if (this.count > this.limit || this.server.connections > this.limit){
-		this.count--;
-		return conn.disconnect();
-	}
-	conn.on('message', this.onMessage.bind(this, conn));
-	conn.on('disconnect', this.onDisconnect.bind(this, conn));
-	conn.on('post',   this.onPost.bind(this, conn));
-	conn.on('delete', this.onDelete.bind(this));
-	conn.on('put',    this.onPut.bind(this, conn));
-	conn.on('remove', this.onRemove.bind(this, conn));
-	conn.on('get',    this.onGet.bind(this, conn));
-	//this.emit('clientConnect', this, conn);
-};
-
-Planet.prototype.onMessage = function(conn, data){
-	this.sockets.emit('message', data);
-	// this.emit('clientMessage', this, conn, data);
-};
-
-Planet.prototype.onDisconnect = function(conn){
-	this.count--;
-	// log(this.count, this.server.connections);
-	// this.emit('clientDisconnect', this, conn);
-};
 
 Planet.prototype.send = function(t, h, i, s){
 	this.sockets.emit(t, h, i, s);
@@ -66,9 +30,45 @@ Planet.prototype.send = function(t, h, i, s){
 	return this;
 };
 
-Planet.prototype.state = {};
+function listen(){
+	var location = this.server.address();
+	log('Planet started at ' + [location.address, location.port].join(':'));
+	this.emit('listening', this, location.address, location.port);
+}
 
-Planet.prototype.onPost = function(conn, data){
+function error(error){
+	log('error', error);
+}
+
+function connect(conn){
+	this.count++;
+	if (this.count > this.limit || this.server.connections > this.limit){
+		this.count--;
+		return conn.disconnect();
+	}
+	conn.on('message', message.bind(this, conn));
+	conn.on('disconnect', disconnect.bind(this, conn));
+	conn.on('post',   onPost.bind(this, conn));
+	conn.on('delete', onDelete.bind(this));
+	conn.on('put',    onPut.bind(this, conn));
+	conn.on('remove', onRemove.bind(this, conn));
+	conn.on('get',    onGet.bind(this, conn));
+	//this.emit('clientConnect', this, conn);
+}
+
+function disconnect(conn){
+	this.count--;
+	// log(this.count, this.server.connections);
+	// this.emit('clientDisconnect', this, conn);
+}
+
+function message(conn, data){
+	this.sockets.emit('message', data);
+	// this.emit('clientMessage', this, conn, data);
+}
+
+
+function onPost(conn, data){
 	if (typeof data != 'object' 
 		|| data == null
 		|| toString.call(data) != '[object Object]'
@@ -77,14 +77,14 @@ Planet.prototype.onPost = function(conn, data){
 	}
 	merge(this.state, data);
 	this.send('post', data);
-};
+}
 
-Planet.prototype.onDelete = function(){
+function onDelete(){
 	this.send('delete');
 	this.state = {};
-};
+}
 
-Planet.prototype.onPut = function(conn, key, value){
+function onPut(conn, key, value){
 	if (value === undefined
 		|| (typeof key != 'string' && !isArray(key))
 		|| (key.length != null && key.length == 0)
@@ -95,9 +95,9 @@ Planet.prototype.onPut = function(conn, key, value){
 	else set(this.state, key, value);
 
 	this.send('put', key, value);
-};
+}
 
-Planet.prototype.onRemove = function(conn, key){
+function onRemove(conn, key){
 	var k, o;
 
 	if (typeof key == 'string'){
@@ -124,9 +124,9 @@ Planet.prototype.onRemove = function(conn, key){
 
 	delete o[k];
 	this.send('remove', key);
-};
+}
 
-Planet.prototype.onGet = function(conn, key, fn){
+function onGet(conn, key, fn){
 	if (typeof key == 'function') fn = key;
 	else if (typeof fn != 'function'
 		|| key == null
@@ -140,7 +140,7 @@ Planet.prototype.onGet = function(conn, key, fn){
 		: isArray(key) ? fn(get(this.state, key))
 		: fn(this.state)
 	);
-};
+}
 
 
 module.exports = Planet;
