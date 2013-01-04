e60a927d22a787aac1e4c088ec62fb2bc9d2205e
tcme <hi@this-connect.me>
2013-01-05 00:10:56 +0100

    api get and put by char number

--- tests/suites/client.array.js
+++ tests/suites/client.array.js
@@ -101,6 +101,61 @@ Tests.describe('Planet Client API: Array', function(it){
 	});
 
 
+	it('should `get` a single char from a string', function(expect){
+		expect.perform(2);
+
+		var socket = io.connect('//:8004', {
+			'force new connection': true
+		});
+
+		socket.on('connect', function(){
+			socket.emit('merge', {
+				'a string': '%is a string%'
+			});
+		});
+
+		socket.on('merge', function(){
+			socket.emit('get', ['a string', 0], function(data){
+				expect(data).toBe('%');
+			});
+			socket.emit('get', ['a string', 1], function(data){
+				expect(data).toBe('i');
+				socket.disconnect();
+			});
+		});
+		
+	});
+
+
+	it('should `set` a single char of a string', function(expect){
+		expect.perform(3);
+
+		var socket = io.connect('//:8004', {
+			'force new connection': true
+		});
+
+		socket.on('connect', function(){
+			socket.emit('delete');
+			socket.emit('merge', {'a string': '%is a string%'});
+			socket.emit('put', ['a string', 2], 'n');
+		});
+
+		socket.on('put', function(){
+			socket.emit('get', 'a string', function(string){
+				expect(string).toBe('%in a string%');
+			});
+			socket.emit('get', ['a string', 2], function(data){
+				expect(data).toBe('n');
+			});
+			socket.emit('get', function(data){
+				expect(Object.keys(data).length).toBe(1);
+				socket.disconnect();
+			});
+		});
+		
+	});
+
+
 
 });
 
