1d3cbe0f3815ded5f8d3376d31c213e9e7d9d0e8
tcme <hi@this-connect.me>
2013-02-25 23:20:57 +0100

    fixes example

---
+++ examples/server.js
@@ -0,0 +1,26 @@
+var connect = require('connect'),
+	io     	= require('socket.io'),
+	planet 	= require('../'),
+	network = require('os').networkInterfaces();
+
+var ip = network.en1[network.en1.length - 1].address;
+
+var server = connect()
+	.use(connect.static(__dirname))
+	.listen(8004, ip);
+
+console.log('\nbrowse to http://' + ip + ':8004/simple.html');
+
+
+var socket = io.listen(server, {
+	'transports': ['websocket'],
+	'flash policy server': false,
+	'log level': 1//,
+	//'browser client': false,
+	//'browser client cache': true,
+	//'browser client minification': true,
+	//'browser client gzip': true
+});
+
+
+planet(socket);
